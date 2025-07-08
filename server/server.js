6fdebe326baa0b79dc9f01fae4927a59ff96c41e
Anmol <anmolkrjha006@gmail.com>
2025-07-08 10:59:01 +0530

    Added port in the backend

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const rooms = {};
 const roomDrawings = {};
